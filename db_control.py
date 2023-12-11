e022854c4c5b46513356ef3fdd64807a895a4ec3
Ilukatsk <128325768+Ilukatsk@users.noreply.github.com>
2023-12-11 10:46:26 +0300

    Подключил БДшку, не тестил пока, но должно работать

---
+++ db_control.py
@@ -0,0 +1,37 @@
+import sqlite3
+import asyncio
+
+def adduser(id : str, db_name : str, table_name : str):
+    sqlite_connection = sqlite3.connect(db_name)
+    cursor = sqlite_connection.cursor()
+    insert_comm = '''INSERT OR IGNORE INTO {} (id)
+                    VALUES ({});
+                    '''.format(table_name, id)
+    cursor.execute(insert_comm)
+    sqlite_connection.commit()
+    cursor.close()
+
+def adduserinfo(id : str, val : str, val_name : str, db_name : str, table_name : str):
+    sqlite_connection = sqlite3.connect(db_name)
+    cursor = sqlite_connection.cursor()
+    insert_comm = '''UPDATE {}
+                    SET {} = {}
+                    WHERE id = {};
+                    '''.format(table_name, val_name,"'" + val + "'", id)
+    print(insert_comm)
+    cursor.execute(insert_comm)
+    sqlite_connection.commit()
+    cursor.close()
+
+
+def getdate(id : str, db_name : str, table_name : str):
+    sqlite_connection = sqlite3.connect(db_name)
+    cursor = sqlite_connection.cursor()
+    insert_comm = '''SELECT birth_day, birth_month
+                    FROM {}
+                    WHERE id = '{}';
+                    '''.format(table_name, id)
+    cursor.execute(insert_comm)
+    output = cursor.fetchall()
+    cursor.close()
+    return output[0]
--- registration.py
+++ registration.py
@@ -2,6 +2,8 @@ from aiogram import Router, types, F, filters
 from aiogram.fsm.state import StatesGroup, State
 from aiogram.methods.send_message import SendMessage
 from aiogram.fsm.context import FSMContext
+from db_control import adduser, adduserinfo, getdate
+from parser_horoscope import request_ru_today_horoscope
 
 
 router = Router()
@@ -48,24 +50,19 @@ class Registration(StatesGroup):
 
 @router.message(filters.Command('start'))
 async def start(message: types.Message, state: FSMContext):
-    try:
-        #profile = {}
-        #profile['id'] = message.from_user.id#тут потом прикручу функцию с табличкой
-        await message.answer("Введите имя")
-        await state.set_state(Registration.filling_first_name)
-    except:
-        None
+    adduser(str(message.from_user.id), 'users.db', 'bot_users')
+    adduserinfo(str(message.from_user.id), str(message.from_user.username), 'username', 'users.db', 'bot_users')
+    await message.answer("Введите имя")
+    await state.set_state(Registration.filling_first_name)
 
 @router.message(Registration.filling_first_name)
 async def first_name(message: types.Message, state: FSMContext):
-    try:
-        first_name = message.text
-        print(first_name)
-        #profile["first_name"] = first_name
-        await message.answer("Введите год рождения")
-        await state.set_state(Registration.filling_birthday_year)
-    except:
-        None
+    first_name = message.text
+    print(first_name)
+    #profile["first_name"] = first_name
+    adduserinfo(str(message.from_user.id), first_name, 'first_name', 'users.db', 'bot_users')
+    await message.answer("Введите год рождения")
+    await state.set_state(Registration.filling_birthday_year)
 
 @router.message(Registration.filling_last_name)
 async def first_name(message: types.Message, state: FSMContext):
@@ -73,26 +70,26 @@ async def first_name(message: types.Message, state: FSMContext):
         last_name = message.text
         print(last_name)
         #profile["first_name"] = first_name
+        adduserinfo(str(message.from_user.id), last_name, 'last_name', 'users.db', 'bot_users')
         await message.answer("Введите год рождения")
         await state.set_state(Registration.filling_birthday_year)
     except:
         None
 
 @router.message(Registration.filling_birthday_year)
-async def first_name(message: types.Message, state: FSMContext):
-    try:
-        year = message.text
-        print(year)
-        #profile["first_name"] = first_name
-        await message.answer("Теперь выберите месяц", reply_markup=months_keyboard())
-        await state.set_state(Registration.filling_birthday_date)
-    except:
-        None
+async def year(message: types.Message, state: FSMContext):
+    year = message.text
+    print(year)
+    #profile["first_name"] = first_name
+    adduserinfo(str(message.from_user.id), year, 'birth_year', 'users.db', 'bot_users')
+    await message.answer("Теперь выберите месяц", reply_markup=months_keyboard())
+    await state.set_state(Registration.filling_birthday_date)
 
 @router.callback_query(F.data.startswith("month_"))
 async def month(callback: types.CallbackQuery):
     month = callback.data[6:]
     print(month)
+    adduserinfo(str(callback.from_user.id), month, 'birth_month', 'users.db', 'bot_users')
     await callback.message.edit_text("Теперь выберите день", reply_markup=days_keyboard())
 
 
@@ -101,10 +98,16 @@ async def month(callback: types.CallbackQuery):
 async def day(callback: types.CallbackQuery, state: FSMContext):
     day = callback.data[4:]
     print(day)
+    adduserinfo(str(callback.from_user.id), day, 'birth_day', 'users.db', 'bot_users')
+    adduserinfo(str(callback.from_user.id), 1, 'finished_reg', 'users.db', 'bot_users')
+    date = getdate(str(callback.from_user.id), 'users.db', 'bot_users')
+    adduserinfo(str(callback.from_user.id), star_sign(date[0], date[1]), 'sign', 'users.db', 'bot_users')
     keyboard = main_keyboard()
     await callback.message.edit_text(text="Спасибо за регистрацию")
     #await SendMessage(id=callback.from_user.id, text="Спасибо за регистрацию")
     await state.clear()
+
+    
 
     
 @router.message(F.text == "Гороскоп на сегодня")
@@ -134,3 +137,31 @@ async def compat(message: types.Message):
         await message.answer("Совместимость")
     except:
         None
+
+
+def star_sign(day, month):  
+    if month == 'Декабрь':  
+        sun_sign = 'sagittarius' if (day < 22) else 'capricorn'  
+    elif month == 'Январь':  
+        sun_sign = 'capricorn' if (day < 20) else 'aquarius'  
+    elif month == 'Февраль':  
+        sun_sign = 'aquarius' if (day < 19) else 'pisces'  
+    elif month == 'Март':  
+        sun_sign = 'pisces' if (day < 21) else 'aries'  
+    elif month == 'Апрель':  
+        sun_sign = 'aries' if (day < 20) else 'taurus'  
+    elif month == 'Май':  
+        sun_sign = 'taurus' if (day < 21) else 'gemini'  
+    elif month == 'Июнь':  
+        sun_sign = 'gemini' if (day < 21) else 'cancer'  
+    elif month == 'Июль':  
+        sun_sign = 'cancer' if (day < 23) else 'leo'  
+    elif month == 'Август':  
+        sun_sign = 'leo' if (day < 23) else 'virgo'  
+    elif month == 'Сентябрь':  
+        sun_sign = 'virgo' if (day < 23) else 'libra'  
+    elif month == 'Октябрь':  
+        sun_sign = 'libra' if (day < 23) else 'scorpio'  
+    elif month == 'Ноябрь':  
+        sun_sign = 'scorpio' if (day < 22) else 'sagittarius'  
+    return sun_sign  
